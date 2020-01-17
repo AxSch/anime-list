18ee20104d3adec6d4c0befbd30c39696ee08c7b
AxSch <alex_schuneman@outlook.com>
2020-01-17 19:52:22 +0000

    Create basic footer component

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMusic, faGhost } from '@fortawesome/free-solid-svg-icons'
+
+
+const Footer = () => {
+    return (
+        <footer className="flex justify-center items-center bg-gray-900 text-white py-3">
+            <p className="">Made with <FontAwesomeIcon className="text-blue-500" icon={faMusic} /> & <FontAwesomeIcon className="text-white" icon={faGhost} /> by AxSch</p>
+        </footer>
+    )
+}
+
+export default Footer
