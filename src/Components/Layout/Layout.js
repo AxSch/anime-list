aedf6a2a32a44e140d60d6c840c3fd63defbf84b
AxSch <alex_schuneman@outlook.com>
2020-01-17 15:48:18 +0000

    Add Layout component

---
+++ src/Components/Layout/Layout.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Toolbar from '../Toolbar/Toolbar'
+
+const Layout = () => {
+    return (
+        <>
+            <Toolbar></Toolbar>
+            <div>Main</div>
+            <div>Footer</div>
+        </>
+    )
+}
+
+export default Layout
