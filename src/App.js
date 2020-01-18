483441b033054a29ea1f934482e713201bcbf4d2
AxSch <alex_schuneman@outlook.com>
2020-01-18 10:00:26 +0000

    Add programmatic routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import Layout from './Components/Layout/Layout'
+import { Switch, Route } from 'react-router-dom'
 
 function App() {
   return (
     <div className="h-screen">
-      <Layout></Layout>
+      <Layout>
+        <Switch>
+          <Route path="/" component={"home"} />
+        </Switch>
+      </Layout>
     </div>
   );
 }
