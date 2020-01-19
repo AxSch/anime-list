b3d7e689b3d08732a42fc3f0989ecb70e68eed0e
AxSch <alex_schuneman@outlook.com>
2020-01-19 13:16:31 +0000

    Refactor path

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import Layout from './Components/Layout/Layout'
 import { Switch, Route } from 'react-router-dom'
+import ListView from './Containers/ListView/ListView'
+
 
 function App() {
   return (
-    <div className="h-screen">
+    <div className="h-screen relative">
       <Layout>
         <Switch>
-          <Route path="/" component={"home"} />
+          <Route path="/" exact component={ListView} />
         </Switch>
       </Layout>
     </div>
