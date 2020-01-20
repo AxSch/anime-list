fbfb04e8097dc5cc3f27779145342deb04b2681a
AxSch <alex_schuneman@outlook.com>
2020-01-20 21:55:14 +0000

    Alter Layout to include Footer component

--- src/Components/Layout/Layout.js
+++ src/Components/Layout/Layout.js
@@ -1,14 +1,19 @@
 import React from 'react'
 import Toolbar from '../Toolbar/Toolbar'
+import Footer from '../Footer/Footer'
 
 const Layout = ({ children }) => {
     return (
-        <div className="relative">
+        <div>
             <div className="fixed top-0 z-10 w-full">
                 <Toolbar />
             </div>
-            <div className="absolute" style={{top: '100px'}}>
+            <div className="mt-40">
                 {children}
+            </div>
+            <div>
+            <Footer />
+                {/* <h1 className="bg-red-700">hi</h1> */}
             </div>
         </div>
     )
