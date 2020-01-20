379936c191827d28dbd79ffc65ff624611ea51b8
AxSch <alex_schuneman@outlook.com>
2020-01-20 21:54:34 +0000

    Remove unnecessary class

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -5,7 +5,7 @@ import { faMusic, faGhost } from '@fortawesome/free-solid-svg-icons'
 
 const Footer = () => {
     return (
-        <footer className="flex justify-center items-center bg-gray-900 text-white py-3">
+        <footer className="flex justify-center bg-gray-900 text-white py-3">
             <p className="">Made with <FontAwesomeIcon className="text-blue-500" icon={faMusic} /> & <FontAwesomeIcon className="text-white" icon={faGhost} /> by AxSch</p>
         </footer>
     )
